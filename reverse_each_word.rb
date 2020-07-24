51b93c3755e78c9dae9f52c336d69b3340951fbd
Mar Mustafa <amustaf@email.arizona.edu>
2020-07-24 16:28:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(phrase)
   phrase.split.map do |word|
     word.reverse
   end
-   p new_array.join(" ")
+   phrase.join(" ")
 end
 
 sentence.split.map {|word| word.reverse}.join(" ")
